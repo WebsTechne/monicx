63602ddccf38163f0dc02fb47c56b7da8e2c0d11
Triumph <triumphaidenojie08@gmail.com>
2025-10-27 14:52:32 +0100

    Fixing build issue

--- src/components/layout/admin/AppSidebar.tsx
+++ src/components/layout/admin/AppSidebar.tsx
@@ -111,7 +111,9 @@ export default function AppSidebar() {
                     </SidebarMenuItem>
                 </SidebarMenu>
             </SidebarHeader>
+
             <SidebarSeparator className="bg-border! mx-0!" />
+
             <SidebarContent className="flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden">
                 <SidebarGroup>
                     <SidebarGroupLabel>Application</SidebarGroupLabel>
@@ -295,6 +297,7 @@ export default function AppSidebar() {
                     </SidebarGroupContent>
                 </SidebarGroup>
             </SidebarContent>
+
             <SidebarFooter className="h-16! overflow-clip">
                 <SidebarMenu>
                     <SidebarMenuItem>

1d7bf15841c5b92cbcca6aa27138dd1899ab8760
Triumph <triumphaidenojie08@gmail.com>
2026-01-01 21:59:28 +0100

    refactor(ui): optimize auth pages by changing logo from png to svg

--- src/app/auth/_components/auth-header.tsx
+++ src/app/auth/_components/auth-header.tsx
@@ -1,18 +1,9 @@
-import IMAGES from "@/assets/images";
 import { ThemeToggle } from "@/components/elements/theme-toggle";
 import { Logo } from "@/components/logo";
 import { Field } from "@/components/ui/field";
-import useMounted from "@/hooks/use-mounted";
-import { useTheme } from "next-themes";
-import Image from "next/image";
 import Link from "next/link";
 
 const AuthHeader = () => {
-  const mounted = useMounted();
-
-  const { resolvedTheme } = useTheme();
-  const isDark = resolvedTheme === "dark";
-
   return (
     <Field
       orientation="horizontal"

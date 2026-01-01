baa4da3f88244e1b8630de5bd44594ce84e2aabd
Triumph <triumphaidenojie08@gmail.com>
2026-01-01 03:41:06 +0100

    refactor(loading) change logo path from light to dark

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -6,7 +6,7 @@ export default function Loading() {
     <div className="bg-background fixed top-0 left-0 z-50 flex h-[100dvh] w-[100vw] items-center justify-center">
       <div className="aspect-1 absolute top-1/2 left-1/2 flex max-w-[150px] -translate-1/2 flex-col items-center">
         <Image
-          src={IMAGES.logo.circleLight}
+          src={IMAGES.logo.circleDark}
           alt="logo"
           width={150}
           height={150}

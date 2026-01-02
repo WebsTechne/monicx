735a9f04be4741f6555ac9b4d307f5be48a3e8d1
Triumph <triumphaidenojie08@gmail.com>
2026-01-02 12:42:48 +0100

    feat(admin): protect admin routes using better-auth session

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,29 +1,32 @@
 import { ReactNode } from "react";
 import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import AppSidebar from "@/components/layout/admin/AppSidebar";
 import AdminHeader from "@/components/layout/admin/AdminHeader";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { SidebarProvider } from "@/components/ui/sidebar";
-import { cookies } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { Toaster } from "@/components/ui/sonner";
 import { getColors } from "@/lib/fetch/get-colors";
 import { getSizes } from "@/lib/fetch/get-sizes";
 import { getCollections } from "@/lib/fetch/get-collections";
 import { getCategories } from "@/lib/fetch/get-categories";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { notFound } from "next/navigation";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-//   display: "swap",
-//   fallback: ["sans-serif"],
-// });
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-//   display: "swap",
-//   fallback: ["monospace"],
-// });
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+  display: "swap",
+  fallback: ["sans-serif"],
+});
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+  display: "swap",
+  fallback: ["monospace"],
+});
 
 export const metadata: Metadata = {
   title: "Admin • Monicx",
@@ -35,6 +38,12 @@ export default async function AdminLayout({
 }: {
   children: ReactNode;
 }) {
+  const session = await auth.api.getSession({ headers: await headers() });
+
+  if (!session?.user) notFound();
+  const user = session?.user;
+  if (user.role !== "admin") notFound();
+
   const cookieStore = await cookies();
   const defaultOpen = cookieStore.get("sidebar_state")?.value === "true";
 
@@ -45,8 +54,8 @@ export default async function AdminLayout({
 
   return (
     <div
-      // className={`${geistSans.variable} ${geistMono.variable} admin-dashboard flex antialiased`}
-      className="admin-dashboard flex antialiased"
+      className={`${geistSans.variable} ${geistMono.variable} admin-dashboard flex antialiased`}
+      // className="admin-dashboard flex antialiased"
     >
       <ThemeProvider
         attribute="class"
